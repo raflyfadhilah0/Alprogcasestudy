ca765b6105bf267960a1689b18677d599075891f
Muhammad Rafly Fadhilah Irsa <raflyyoungrenirsa@gmail.com>
2025-05-21 16:41:13 +0700

    main.cpp

--- main.cpp
+++ main.cpp
@@ -6,5 +6,10 @@ int main(){
     std::cout << "Aku Raja Kau ratu" << std::endl;
     std::cout << "bagus" << std::endl;
      std::cout << "Alprog naga langit" << std::endl;
+    // nama dan npm
+     std::cout << "Muhammad Rafly Fadhilah Irsa - 2306247566" << std::endl;
+    std::cout << "Muhamad Asrhal Rizaldi - 2306156744" << std::endl;
+    std::cout << "Mikhail Ayares - 2306210456" << std::endl;
+    std::cout << "Keefe Razzan Hidayat - 2306217790" << std::endl;
     return 0;
 }
