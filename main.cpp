29c9e72295b9c71a55972b66b1607b92595628d8
Muhammad Rafly Fadhilah Irsa <raflyyoungrenirsa@gmail.com>
2025-05-21 16:23:40 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <stdio.h>
 #include <iostream>
 int main(){
-    std::cout << "hello world" << std::endl;
+    std::cout << "hello bro" << std::endl;
     std::cout << "halo mik" << std::endl;
+    std::cout << "halo kef" << std::endl;
+    std::cout << "halo arshal" << std::endl;
+    
     return 0;
 }
